d1d1a8837efafdfb9fd3e353a2c2c1b9fbc06597
davistheweb <josiahdave001@gmail.com>
2026-04-28 11:01:14 +0100

    :

--- src/components/navbar/NavLinks.tsx
+++ src/components/navbar/NavLinks.tsx
@@ -14,7 +14,7 @@ export const NavLinks: React.FC<INavLinksProps> = ({
       <Link
         href={href}
         onClick={handleNavClick}
-        className={`${isActive ? "lg:after:absolute after:w-18 after:h-0.75 after:bg-[#F97316] after:bottom-0 after:content[''] ease-in duration-500 transition-all after:left-0 after:right-0" : "lg:after:absolute after:w-18 hover:after:h-0.75 hover:after:bg-[#F97316] hover:after:bottom-0 hover:after:content[''] ease-in hover:duration-500 hover:transition-all hover:after:left-0 hover:after:right-0"}`}
+        className={`lg:after:absolute after:w-18 after:h-0.75 text-[14px] xl:text-[16px] ${isActive ? " after:bg-[#F97316] after:bottom-0 after:content[''] ease-in duration-500 transition-all after:left-0 after:right-0" : " hover:after:bg-[#F97316] hover:after:bottom-0 hover:after:content[''] ease-in hover:duration-500 hover:transition-all hover:after:left-0 hover:after:right-0"}`}
       >
         {children}
       </Link>
