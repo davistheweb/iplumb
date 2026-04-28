d74ff15779a21a529efa8df9068dfba976d4437b
davistheweb <josiahdave001@gmail.com>
2026-04-28 17:47:59 +0100

    updated reachout component

--- src/components/ui/ReachOut.tsx
+++ src/components/ui/ReachOut.tsx
@@ -7,7 +7,7 @@ export const ReachOut: React.FC = () => {
       className="bg-[#EF8300] w-16 h-16 rounded-full fixed bottom-10 flex items-center justify-center right-5"
       href="#"
     >
-      <MessageSquare color="white" size={25} />
+      <MessageSquare color="white" fill="white" size={25} />
     </a>
   );
 };
