d1686615e3ebf8766ae19e38afb5cb608c8a35e8
davistheweb <josiahdave001@gmail.com>
2026-05-02 09:20:01 +0100

    ux fix

--- src/components/about/ReachOutSection.tsx
+++ src/components/about/ReachOutSection.tsx
@@ -2,7 +2,7 @@ export const ReachOutSection: React.FC = () => {
   return (
     <section className="flex items-center justify-center px-8 py-10">
       <div className="md:w-165.2 flex h-fit w-87.5 flex-col rounded-2xl bg-[#1B365D] sm:w-125 md:flex-row lg:w-250 xl:w-304">
-        <div className="flex flex-col gap-4 px-20 py-20">
+        <div className="flex flex-col gap-4 px-8 py-8 md:px-20 md:py-20">
           <h3 className="font-manrope text-white">
             Ready for Professional Standards?
           </h3>
@@ -14,13 +14,13 @@ export const ReachOutSection: React.FC = () => {
           <div className="flex flex-col gap-4 lg:flex-row">
             <a
               href="#"
-              className="w-fit rounded-xl bg-[#FD6673] px-12 py-4.5 text-white"
+              className="w-fit rounded-xl bg-[#FD6673] px-8 py-4.5 text-white md:px-12"
             >
               Get a Custom Quote
             </a>
             <a
               href="#"
-              className="w-fit rounded-xl border border-[#87A0CD] px-12 py-4.5 text-white"
+              className="w-fit rounded-xl border border-[#87A0CD] px-4 py-4.5 text-white md:px-12"
             >
               Speak with a Consultant
             </a>
